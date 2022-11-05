9d4e2b6a9b728e065b927be8c466a4a9b5260fd0
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2022-11-05 14:23:26 -0300

    feat: shows qt chart

--- source/main-window.cpp
+++ source/main-window.cpp
@@ -1,4 +1,61 @@
 #include "main-window.hpp"
 
-Ventilator::Ventilator(QWidget * parent) : QMainWindow(parent) {}
-Ventilator::~Ventilator() {}
+#include <iostream>
+#include <QChartView>
+#include <QTimer>
+#include <QValueAxis>
+#include <QVBoxLayout>
+
+Widget::Widget(QWidget * parent)
+    : QWidget(parent)
+    , chart_(new QChart)
+    , series_(new QLineSeries)
+    , counter_(0)
+{
+    chart_->addSeries(series_);
+
+    QChartView * view = new QChartView(chart_);
+    view->setMinimumSize(800, 600);
+
+    QValueAxis * xaxis = new QValueAxis;
+    QValueAxis * yaxis = new QValueAxis;
+
+    xaxis->setRange(0, 100);
+    xaxis->setLabelFormat("%g");
+    xaxis->setTitleText("Samples");
+
+    yaxis->setRange(0, 200);
+    yaxis->setTitleText("Audio Level");
+
+    chart_->addAxis(xaxis, Qt::AlignBottom);
+    chart_->addAxis(yaxis, Qt::AlignLeft);
+
+    QVBoxLayout * layout = new QVBoxLayout(this);
+    layout->addWidget(view);
+
+    QTimer * timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Widget::update);
+    timer->start(10);
+}
+
+Widget::~Widget() {}
+
+void
+Widget::update() {
+    if (ps_.isEmpty()) {
+        ps_.reserve(100);
+        for (std::size_t i = 0; i < 100; i++) { ps_.append(QPointF(i, 1)); }
+    }
+    series_->replace(ps_);
+}
+
+
+Ventilator::Ventilator(QWidget * parent)
+    : QMainWindow(parent)
+{
+    widget_ = new Widget();
+    setCentralWidget(widget_);
+}
+
+Ventilator::~Ventilator()
+{}
--- source/main-window.hpp
+++ source/main-window.hpp
@@ -1,7 +1,27 @@
 #ifndef VENTILATIOR_WINDOW_HPP__
 #define VENTILATIOR_WINDOW_HPP__
 
+
+#include <QChart>
+#include <QLineSeries>
 #include <QMainWindow>
+#include <QPointF>
+#include <QVector>
+
+class Widget : public QWidget {
+    Q_OBJECT
+    public:
+        Widget(QWidget * parent = 0);
+        ~Widget();
+    public slots:
+        void update();
+    private:
+        QChart *            chart_;
+        QLineSeries *       series_;
+        QVector<QPointF>    ps_;
+
+        std::size_t counter_;
+};
 
 class Ventilator : public QMainWindow {
     Q_OBJECT
@@ -9,6 +29,7 @@ class Ventilator : public QMainWindow {
         Ventilator(QWidget * parent = 0);
         ~Ventilator();
     private:
+        Widget * widget_;
 };
 
 #endif // VENTILATIOR_WINDOW_HPP__
